4f8e83ec1cb135187cdfbb0cf52516491748a85e
Farrukh <adler4313@gmail.com>
2024-08-15 00:20:13 +0500

    added content navbar

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
 
 declare(strict_types=1);
-
-
 require 'vendor/autoload.php';
-require 'bootsrap.php';
+require 'bootstrap.php';
 
 $user = new \App\User();
 
@@ -16,14 +14,14 @@ $status = new \App\Status();
 
 //var_dump($User->getUser(2));
 
-//$ads->create('ads', 'malumot', 1, 1,1, 'Samarqand', 50, 3);
+$ads->create('ads', 'malumot', 1, 1,1, 'Samarqand', 50, 3);
 //var_dump($ads->delete(6));
 
 //$branch->update(1,'ads', 'Fargona');
 //var_dump($branch->delete(1));
 
 //$status->update(1,'Sarvar');
-var_dump($status->delete(1));
+// var_dump($status->delete(1));
 ?>
 
 
--- src/Ads.php
+++ src/Ads.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 namespace App;
+
 use PDO;
 
 class Ads
@@ -41,45 +42,46 @@ class Ads
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function get($id): array{
-        $query = "SELECT * FROM ads WHERE id = :id";
-        $stmt = $this->pdo->prepare($query);
-        $stmt->bindParam(':id', $id);
-        $stmt->execute();
-
-        return $stmt->fetch(PDO::FETCH_ASSOC);
+    public function getAds(): array{
+        $query = "SELECT *, ads.address AS address FROM ads JOIN branch ON branch.id  = ads.branch_id";
+        return $this->pdo->query($query)->fetchAll();
     }
 
-    public function update(
-        int $id,
-        string $title,
-        string $description,
-        int $user_id,
-        int $status_id,
-        int $branch_id,
-        string $address,
-        float $price,
-        int $rooms
-    )
-    {
-        $query = "UPDATE ads SET title = :title, description = :description, user_id = :user_id,
-                 status_id = :status_id, branch_id = :branch_id, address = :address, 
-                 price = :price, rooms = :rooms, updated_at = NOW() WHERE id = :id";
-
-        $stmt = $this->pdo->prepare($query);
-        $stmt->bindParam(':id', $id);
-        $stmt->bindParam(':title', $title);
-        $stmt->bindParam(':description', $description);
-        $stmt->bindParam(':user_id', $user_id);
-        $stmt->bindParam(':status_id', $status_id);
-        $stmt->bindParam(':branch_id', $branch_id);
-        $stmt->bindParam(':address', $address);
-        $stmt->bindParam(':price', $price);
-        $stmt->bindParam(':rooms', $rooms);
-        $stmt->execute();
-
-        return $stmt->fetch(PDO::FETCH_ASSOC);
-    }
+//    public function update(
+//        int $id,
+//        string $title,
+//        string $description,
+//        int $user_id,
+//        int $status_id,
+//        int $branch_id,
+//        string $address,
+//        float $price,
+//        int $rooms
+//    )
+//    {
+//        $query = "UPDATE CREATE TABLE advertising (
+//                                 id INT AUTO_INCREMENT PRIMARY KEY,
+//                                 text varchar(500),
+//                                 created_at TIMESTAMP
+//);
+//ds SET title = :title, description = :description, user_id = :user_id,
+//                 status_id = :status_id, branch_id = :branch_id, address = :address,
+//                 price = :price, rooms = :rooms, updated_at = NOW() WHERE id = :id";
+//
+//        $stmt = $this->pdo->prepare($query);
+//        $stmt->bindParam(':id', $id);
+//        $stmt->bindParam(':title', $title);
+//        $stmt->bindParam(':description', $description);
+//        $stmt->bindParam(':user_id', $user_id);
+//        $stmt->bindParam(':status_id', $status_id);
+//        $stmt->bindParam(':branch_id', $branch_id);
+//        $stmt->bindParam(':address', $address);
+//        $stmt->bindParam(':price', $price);
+//        $stmt->bindParam(':rooms', $rooms);
+//        $stmt->execute();
+//
+//        return $stmt->fetch(PDO::FETCH_ASSOC);
+//    }
 
     public function delete(int $id): array|false
     {
